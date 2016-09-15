8fa11700acd0c4553471fab642ba36d1e7a05010
mrexodia <mr.exodia.tpodt@gmail.com>
2016-09-15 23:51:26 +0200

    initial commit

---
+++ Overbook/Feed.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace Overbook
+{
+    [XmlRoot(ElementName = "link", Namespace = "http://www.w3.org/2005/Atom")]
+    public class Link
+    {
+        [XmlAttribute(AttributeName = "href")]
+        public string Href { get; set; }
+
+        [XmlAttribute(AttributeName = "rel")]
+        public string Rel { get; set; }
+
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "owner", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+    public class Owner
+    {
+        [XmlElement(ElementName = "name", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "email", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string Email { get; set; }
+    }
+
+    [XmlRoot(ElementName = "image", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+    public class Image
+    {
+        [XmlAttribute(AttributeName = "href")]
+        public string Href { get; set; }
+    }
+
+    [XmlRoot(ElementName = "link", Namespace = "http://www.w3.org/2005/Atom")]
+    public class AtomLink
+    {
+        [XmlAttribute(AttributeName = "href")]
+        public string Href { get; set; }
+
+        [XmlAttribute(AttributeName = "rel")]
+        public string Rel { get; set; }
+
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "category", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+    public class Category
+    {
+        [XmlAttribute(AttributeName = "text")]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "enclosure")]
+    public class Enclosure
+    {
+        [XmlAttribute(AttributeName = "url")]
+        public string Url { get; set; }
+
+        [XmlAttribute(AttributeName = "length")]
+        public string Length { get; set; }
+
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "guid")]
+    public class Guid
+    {
+        [XmlAttribute(AttributeName = "isPermaLink")]
+        public string IsPermaLink { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+
+        [XmlElement(ElementName = "link")]
+        public string Link { get; set; }
+
+        [XmlElement(ElementName = "pubDate")]
+        public string PubDate { get; set; }
+
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+
+        [XmlElement(ElementName = "enclosure")]
+        public Enclosure Enclosure { get; set; }
+
+        [XmlElement(ElementName = "summary", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string Summary { get; set; }
+
+        [XmlElement(ElementName = "image", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public Image Image { get; set; }
+
+        [XmlElement(ElementName = "guid")]
+        public Guid Guid { get; set; }
+    }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+
+        [XmlElement(ElementName = "link")]
+        public string Link { get; set; }
+
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+
+        [XmlElement(ElementName = "language")]
+        public string Language { get; set; }
+
+        [XmlElement(ElementName = "copyright")]
+        public string Copyright { get; set; }
+
+        [XmlElement(ElementName = "link", Namespace = "http://www.w3.org/2005/Atom")]
+        public AtomLink AtomLink { get; set; }
+
+        [XmlElement(ElementName = "lastBuildDate")]
+        public string LastBuildDate { get; set; }
+
+        [XmlElement(ElementName = "author", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string Author { get; set; }
+
+        [XmlElement(ElementName = "summary", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string Summary { get; set; }
+
+        [XmlElement(ElementName = "owner", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public Owner Owner { get; set; }
+
+        [XmlElement(ElementName = "explicit", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string Explicit { get; set; }
+
+        [XmlElement(ElementName = "image", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public Image Image { get; set; }
+
+        [XmlElement(ElementName = "category", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public Category Category { get; set; }
+
+        [XmlElement(ElementName = "item")]
+        public List<Item> Items { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rss")]
+    public class Rss
+    {
+        [XmlElement(ElementName = "channel")]
+        public Channel Channel { get; set; }
+
+        [XmlAttribute(AttributeName = "atom", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Atom { get; set; }
+
+        [XmlAttribute(AttributeName = "itunes", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Itunes { get; set; }
+
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+    }
+}
---
+++ Overbook/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace Overbook
+{
+    class Program
+    {
+        public class Utf8StringWriter : StringWriter
+        {
+            public override Encoding Encoding { get { return Encoding.UTF8; } }
+        }
+
+        public static string Serialize<T>(T value)
+        {
+            if (value == null)
+                return null;
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            var ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            var settings = new XmlWriterSettings();
+            settings.Encoding = new UTF8Encoding(false, false);
+            settings.Indent = false;
+            settings.OmitXmlDeclaration = false;
+            settings.Indent = true;
+
+            using (var textWriter = new Utf8StringWriter())
+            {
+                using (var xmlWriter = XmlWriter.Create(textWriter, settings))
+                {
+                    serializer.Serialize(xmlWriter, value, ns);
+                }
+                return textWriter.ToString();
+            }
+        }
+
+        public static T Deserialize<T>(string xml)
+        {
+            if (string.IsNullOrEmpty(xml))
+                return default(T);
+
+            var serializer = new XmlSerializer(typeof(T));
+
+            var settings = new XmlReaderSettings();
+            // No settings need modifying here
+
+            using (var textReader = new StringReader(xml))
+            {
+                using (var xmlReader = XmlReader.Create(textReader, settings))
+                {
+                    return (T)serializer.Deserialize(xmlReader);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var items = new List<Item>();
+            items.Add(new Item
+            {
+                Title = "Our first Item",
+                Link = "http://testing.com/Our_first_item.mp3",
+                PubDate = "Wed, 13 Aug 2014 15:47:00 GMT",
+                Description = "This is our first item in our feed",
+                Enclosure = new Enclosure
+                {
+                    Url = "http://files.idrsolutions.com/Java_PDF_Podcasts/Interview_4_Advice_and_XFA.mp3",
+                    Length = "11779397",
+                    Type = "audio/mpeg"
+                },
+                Summary = "Our fiest item",
+                Image = new Image
+                {
+                    Href = "http://files.idrsolutions.com/Java_PDF_Podcasts/idrlogo.png"
+                },
+                Guid = new Guid
+                {
+                    IsPermaLink = "false",
+                    Text = "ce094c6b-4918-4833-a3fc-c466b3431cd0"
+                }
+            });
+            var rss = new Rss
+            {
+                Atom = "http://www.w3.org/2005/Atom",
+                Itunes = "http://www.itunes.com/dtds/podcast-1.0.dtd",
+                Version = "2.0",
+                Channel = new Channel
+                {
+                    Title = "This is our Feed title",
+                    Link = "http://www.idrsolutions.com",
+                    Description = "This is a breif description of our podcast",
+                    Language = "en-us",
+                    Copyright = "IDRSolutions copyright 2014",
+                    AtomLink = new AtomLink
+                    {
+                        Href = "https://ogilvie.gq/overbook/feed.xml",
+                        Rel = "self",
+                        Type = "application/rss+xml"
+                    },
+                    LastBuildDate = "Wed, 13 Aug 2014 15:47:00 GMT",
+                    Author = "IDRSolutions",
+                    Summary = "Our First itunes feed",
+                    Owner = new Owner
+                    {
+                        Name = "IDRSolutions",
+                        Email = "contact2007@idrsolutions.com"
+                    },
+                    Explicit = "No",
+                    Image = new Image
+                    {
+                        Href = "http://files.idrsolutions.com/Java_PDF_Podcasts/idrlogo.png"
+                    },
+                    Category = new Category
+                    {
+                        Text = "Technology"
+                    },
+                    Items = items
+                }
+            };
+            var xml = Serialize(rss);
+            Console.WriteLine(xml.Replace("\r", ""));
+        }
+    }
+}
